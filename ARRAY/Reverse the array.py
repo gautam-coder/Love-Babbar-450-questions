86921910b420091a82f8abed7cdeb5fa143b5fd6
gautam-coder <53681420+gautam-coder@users.noreply.github.com>
2022-01-28 15:24:10 +0530

    Add files via upload

--- ARRAY/Reverse the array.py
+++ ARRAY/Reverse the array.py
@@ -1,1 +1,11 @@
-Given an array (or string), the task is to reverse the array/string in Python.
+def reverse(arr):
+    start=0
+    end=len(arr)-1
+    while start<end:
+        arr[start],arr[end]=arr[end],arr[start]
+        start+=1
+        end-=1
+    return arr
+
+s=list(map(int,input().split()))
+print(reverse(s))
